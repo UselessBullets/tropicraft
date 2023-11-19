5ad267730bc2dce72b7dcade994a2eef16b42c45
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-18 22:29:11 -0600

    /

--- src/main/java/cookie/tropicraft/Tropicraft.java
+++ src/main/java/cookie/tropicraft/Tropicraft.java
@@ -1,6 +1,7 @@
 package cookie.tropicraft;
 
 import cookie.tropicraft.block.TropicraftBlocks;
+import cookie.tropicraft.entity.EntityCoconut;
 import cookie.tropicraft.entity.TropicraftEntities;
 import cookie.tropicraft.item.TropicraftItems;
 import cookie.tropicraft.util.ContainerPlayerExtender;
@@ -8,15 +9,27 @@ import cookie.tropicraft.world.BiomeProviderTropics;
 import cookie.tropicraft.world.WorldTypeTropics;
 import cookie.tropicraft.world.biome.TropicraftBiomes;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.minecraft.client.render.entity.SnowballRenderer;
 import net.minecraft.core.world.Dimension;
 import net.minecraft.core.world.type.WorldType;
 import net.minecraft.core.world.type.WorldTypes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.helper.EntityHelper;
 import turniplabs.halplibe.helper.SoundHelper;
 import turniplabs.halplibe.helper.TextureHelper;
 
-public class Tropicraft implements ModInitializer {
+public class Tropicraft implements ModInitializer, PreLaunchEntrypoint {
+	static {
+		// DO NOT TOUCH THIS! It's an error prevention method. Thanks Useless!
+		try {
+			Class.forName("turniplabs.halplibe.HalpLibe");
+			Class.forName("net.minecraft.core.block.Block");
+			Class.forName("net.minecraft.core.item.Item");
+			Class.forName("net.minecraft.core.world.Dimension");
+		} catch (ClassNotFoundException ignored) {}
+	}
     public static final String MOD_ID = "tropicraft";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
@@ -33,34 +46,30 @@ public class Tropicraft implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
-        // DO NOT TOUCH THIS! It's an error prevention method. Thanks Useless!
-        try {
-            Class.forName("net.minecraft.core.block.Block");
-            Class.forName("net.minecraft.core.item.Item");
-        } catch (ClassNotFoundException ignored) {}
-
-		new TropicraftConfig();
-
 		SoundHelper.addSound(MOD_ID, "bonk.wav");
         SoundHelper.addSound(MOD_ID, "gulp.wav");
 		SoundHelper.addSound(MOD_ID, "frog.wav");
 		SoundHelper.addSound(MOD_ID, "boom.wav");
 
-        new TropicraftBlocks().initializeBlocks();
-        new TropicraftItems().initializeItems();
-		new TropicraftEntities().initializeEntities();
-		new TropicraftBiomes().initializeBiomes();
+		EntityHelper.createEntity(EntityCoconut.class, new SnowballRenderer(TropicraftItems.itemCoconut.getIconFromDamage(0)), 125, "coconut");
 
-		BiomeProviderTropics.init();
-
-		tropicsWorld = WorldTypes.register("worldtype.tropicraft.tropics", new WorldTypeTropics("tropicraft.worldtype.tropics"));
-		tropicsDimension = new Dimension("tropicraft.tropics", Dimension.overworld, 1.0f, TropicraftBlocks.tropicsPortal.id);
-		tropicsDimension.setDefaultWorldType(tropicsWorld);
-		Dimension.registerDimension(TropicraftConfig.cfg.getInt("Tropicraft.tropicsDimID"), tropicsDimension);
-
-		new ContainerPlayerExtender().initializeExtender();
+        new ContainerPlayerExtender().initializeExtender();
 
         LOGGER.info("Tropicraft initialized. Have a happy vacation!");
     }
+
+	@Override
+	public void onPreLaunch() {
+		new TropicraftConfig();
+		BiomeProviderTropics.init();
+
+		new TropicraftBlocks().initializeBlocks();
+		new TropicraftItems().initializeItems();
+		new TropicraftBiomes().initializeBiomes();
+
+		tropicsWorld = WorldTypes.register("tropicraft.worldtype.tropics", new WorldTypeTropics("tropicraft.worldtype.tropics"));
+		tropicsDimension = new Dimension("tropicraft.dimension.tropics", Dimension.overworld, 1.0f, TropicraftBlocks.tropicsPortal.id);
+		tropicsDimension.setDefaultWorldType(tropicsWorld);
+		Dimension.registerDimension(TropicraftConfig.cfg.getInt("Tropicraft.tropicsDimID"), tropicsDimension);
+	}
 }
--- src/main/java/cookie/tropicraft/entity/render/RenderFrogPoison.java
+++ src/main/java/cookie/tropicraft/entity/render/RenderFrogPoison.java
@@ -1,8 +1,5 @@
 package cookie.tropicraft.entity.render;
 
-import cookie.tropicraft.entity.EntityFrog;
-import cookie.tropicraft.entity.EntityFrogPoison;
-import net.minecraft.client.render.entity.LivingRenderer;
 import net.minecraft.client.render.model.ModelBase;
 
 public class RenderFrogPoison extends LivingRenderer<EntityFrogPoison> {
